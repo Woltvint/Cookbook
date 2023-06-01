61d6ce3ed4c263f6dfe3c9be325c1f4b2a78fc94
Mikuláš Heřman <her.mikiherman@gmail.com>
2023-06-01 18:02:47 +0200

    inserting recept into database

--- db/database.sql
+++ db/database.sql
@@ -3,24 +3,33 @@ CREATE TABLE labels (
   name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE ingredients (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  units VARCHAR(45)
-);
 
 CREATE TABLE recept (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  img BLOB,
-  labels_id INTEGER,
+  name VARCHAR(255),
+  img VARCHAR(45),
+  labels_id VARCHAR(45),
   summary TEXT,
   ingredients_id VARCHAR(45),
-  name VARCHAR(255),
   howTo TEXT,
-  totalTime BIGINT,
-  servings BIGINT,
-  FOREIGN KEY (labels_id) REFERENCES labels(id),
-  FOREIGN KEY (ingredients_id) REFERENCES ingredients(id)
+  totalTime INT,
+  servings INT,
+  FOREIGN KEY (labels_id) REFERENCES labels(id)
 );
 
+CREATE TABLE ingredients (
+  receprt_name VARCHAR(255),
+  name VARCHAR(255) NOT NULL,
+  ingredient_values INT,
+  units VARCHAR(45)
+);
+
+INSERT INTO labels (name)
+VALUES ("snídaně"),("oběd"),("večeře"),("pití");
+
+INSERT INTO recept (name, img, labels_id, summary, ingredients_id, howTo, totalTime, servings )
+VALUES ("tortila","toritla.jpg","2,3","výborná tortila :d","mouka,voda,olej","smíchej a rozválej :D", 30, 2)
+
+INSERT INTO ingredients (recept_id, name, ingredient_values, units)
+VALUES ("tortila","mouka",100,"g"),("tortila","voda",50,"ml"),("tortila","olej",10,"ml"),
 
